2367eff3462bed32c8b1f0ab564f265451563766
Nour Majdoub <154745715+NoorMajdoub@users.noreply.github.com>
2024-10-31 22:53:40 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,17 @@
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from combinedmodel import *
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
-@app.route("/chatbot")
-def chatbot():
-    return {"members": ["Yourr medical report indicates a high risk of Cardiomegaly Disease , the use of Ibuprofen might have negative impact on your health, please refer to your doctor before consuming Ibuprofen"]}
+
 @app.route("/diagnosis",methods=["POST"])
 def diagnosis():
     data = request.get_json()
     res = "Response from diagnosis"
     user_input = data.get("input")
-    return {"diagnosis": user_input}
+    res=combinedresult(user_input)
+    return {"diagnosis": res}
 if __name__ == "__main__":
     app.run(debug=True)
